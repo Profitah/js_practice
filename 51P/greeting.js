fbc9e42046840d3e747d5272223b3ddeddaa37ab
profitah <qwer120542@gmail.com>
2023-10-26 21:35:55 +0900

    prompt, alert, console.log, write 기능 학습

---
+++ 51P/greeting.js
@@ -0,0 +1,8 @@
+let Name = prompt("이름을 입력하세요"); // 함수 Name은 프롬프트를 출력
+alert(Name +"님, 환영합니다."); // alert함수를 이용해 알림창에 문장출력
+console.log("보고싶었어요"); // 웹브라우저 개발자 console창에 문장출력
+
+/*원코드
+let Name = prompt("이름을 입력하세요"); 
+document.write("<b><big>" + Name +"</big></b>님, 환영합니다."); // write함수를 이용해 이 document에 문장출력(글자 스타일 b,big도 적용)
+*/
